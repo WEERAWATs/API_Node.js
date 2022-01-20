3878e2611711c06deae4ba9b0842fd50ceb05e42
vee.ws <74323401+Kuuvee@users.noreply.github.com>
2022-01-20 14:08:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,17 @@ let PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 // homepage route
 app.get('/', (req, res) => {
     return res.send({
-        error: false,
         message: 'Welcome to RESTful CRUD API with NodeJS, Express, MYSQL',
         written_by: 'WEERAWAT.',
         published_on: 'https://vee_ws.dev',
